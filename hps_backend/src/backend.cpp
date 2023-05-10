fa4d6529a1581c17067e55daf5e9a84c23c59e7b
yingcanw <yingcanw@nvidia.com>
2023-05-09 20:59:41 -0700

    fix log inssue

--- hps_backend/src/backend.cpp
+++ hps_backend/src/backend.cpp
@@ -489,6 +489,12 @@ HPSBackend::ParseParameterServer(const std::string& path)
     }
     HPS_TRITON_LOG(INFO, log_prefix, "the embedding cache type = ", cache_type);
 
+    key = "init_ec";
+    params.init_ec = true;
+    RETURN_IF_ERROR(
+        TritonJsonHelper::parse(params.init_ec, json_obj, key, true));
+    HPS_TRITON_LOG(INFO, log_prefix, "init_ec = ", params.init_ec);
+
     // TODO: Move to paramter server common parameters?
     params.volatile_db = volatile_db_params;
     params.persistent_db = persistent_db_params;
